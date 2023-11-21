eb212ea4504de0dd5597afb158a3f14429353fe8
robinrunfordream <robin30803@gmail.com>
2023-11-21 13:58:54 +0800

    Creat array_1.py
    array test

---
+++ array_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# 使用NumPy建立一個2D數組
+array = np.array([[1,2,3],[4,5,6]])
+
+# 將數組印出
+print(array)
+print('number of dim',array.ndim)
+print('shape',array.shape)
+print('size',array.size)
+
+
+##
---
+++ array_2.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+##a = np.array([2,23,4], dtype=np.int64)
+
+a = np.ones((3,4), dtype=np.int16)
+b = np.empty((3,4))
+
+print(a)
+print(b)
+##print(a.dtype)
