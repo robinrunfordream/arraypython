e62ccaa7070d441a8b7f9c5b005074a157d102ab
robinrunfordream <robin30803@gmail.com>
2023-11-27 14:39:18 +0800

    delete link

--- pandas_plot.py
+++ pandas_plot.py
@@ -1,11 +1,3 @@
-# View more python tutorials on my Youtube and Youku channel!!!
-
-# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
-# Youku video tutorial: http://i.youku.com/pythontutorial
-
-"""
-Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
-"""
 from __future__ import print_function
 import pandas as pd
 import numpy as np
